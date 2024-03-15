38f30d0fe39328d444e8bbcee038244c5eb0d45b
Akame <matthew70306@gmail.com>
2024-03-15 23:40:18 +0900

    add url support and downloading hint

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
  */
 
 import { Router } from 'itty-router';
-var env = null;
+let env = null;
 // Create a new router
 const router = Router();
 
@@ -18,36 +18,8 @@ function validUserId(userId) {
 	return userId == env.USER_ID;
 }
 
-async function tgPutFile(fileId,chatId,messageId) {
-	var res1 = await fetch(
-		`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/getfile?file_id=${fileId}`
-	);
-	var j = await res1.json();
-	if(j["ok"] === false) {
-		await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendmessage`,
-			{
-				"method": "POST",
-				"headers": {
-					"Content-Type": "application/json"
-				},
-				"body": JSON.stringify({
-					"chat_id": chatId,
-					"text": j["description"],
-					"reply_parameters": {
-						"message_id": messageId
-					}
-				})
-			}
-		);
-		return;
-	}
-	console.log(j);
-	var path = `https://api.telegram.org/file/bot${env.TELEGRAM_BOT_TOKEN}/${j["result"]["file_path"]}`;
-	var url = new URL(path);
-	var ext = url.pathname.split(".").pop();
-	var fileName = `${crypto.randomUUID()}.${ext}`;
-	var response = await fetch(path);
-	await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendmessage`,
+async function urlPutFile(url, chatId, messageId) {
+	let msg = await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendmessage`,
 		{
 			"method": "POST",
 			"headers": {
@@ -55,11 +27,45 @@ async function tgPutFile(fileId,chatId,messageId) {
 			},
 			"body": JSON.stringify({
 				"chat_id": chatId,
-				"text": `\`${env.DOMAIN_NAME}/${fileName}\``,
-				"parse_mode": "markdown",
+				"text": "downloading...",
 				"reply_parameters": {
 					"message_id": messageId
+				}
+			})
+		}
+	).then((res) => {
+		return res.json();
+	});
+	let msgId = msg.result.message_id;
+	let response = await fetch(url);
+	if (response.status !== 200 ) {
+		await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/editmessagetext`,
+			{
+				"method": "POST",
+				"headers": {
+					"Content-Type": "application/json"
 				},
+				"body": JSON.stringify({
+					"chat_id": chatId,
+					"message_id": msgId,
+					"text": `Error: status code ${response.status}\``
+				})
+			}
+		);
+		return;
+	}
+	let fileName = `${crypto.randomUUID()}`;
+	await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/editmessagetext`,
+		{
+			"method": "POST",
+			"headers": {
+				"Content-Type": "application/json"
+			},
+			"body": JSON.stringify({
+				"chat_id": chatId,
+				"message_id": msgId,
+				"text": `\`${env.DOMAIN_NAME}/${fileName}\``,
+				"parse_mode": "markdown",
 				"reply_markup": {
 					"inline_keyboard": [[
 						{
@@ -75,20 +81,114 @@ async function tgPutFile(fileId,chatId,messageId) {
 			})
 		}
 	);
-	await env.images.put(fileName,response.body)
+	await env.images.put(fileName, response.body);
 }
 
-async function tgDeleteFile(replyText,replyChatId,replyMessageId,replyUserId) {
+async function tgPutFile(fileId, chatId, messageId) {
+	let res1 = await fetch(
+		`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/getfile?file_id=${fileId}`
+	);
+	let j = await res1.json();
+	if (j.ok === false) {
+		await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendmessage`,
+			{
+				"method": "POST",
+				"headers": {
+					"Content-Type": "application/json"
+				},
+				"body": JSON.stringify({
+					"chat_id": chatId,
+					"text": j.description,
+					"reply_parameters": {
+						"message_id": messageId
+					}
+				})
+			}
+		);
+		return;
+	}
+	let msg = await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendmessage`,
+		{
+			"method": "POST",
+			"headers": {
+				"Content-Type": "application/json"
+			},
+			"body": JSON.stringify({
+				"chat_id": chatId,
+				"text": "downloading...",
+				"reply_parameters": {
+					"message_id": messageId
+				}
+			})
+		}
+	).then((res) => {
+		return res.json();
+	});
+
+	let msgId = msg.result.message_id;
+	let path = `https://api.telegram.org/file/bot${env.TELEGRAM_BOT_TOKEN}/${j.result.file_path}`;
+	let url = new URL(path);
+	let ext = url.pathname.split(".").pop();
+	let fileName = `${crypto.randomUUID()}.${ext}`;
+	try {
+		let response = await fetch(path);
+	} catch (e) {
+		await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/editmessagetext`,
+			{
+				"method": "POST",
+				"headers": {
+					"Content-Type": "application/json"
+				},
+				"body": JSON.stringify({
+					"chat_id": chatId,
+					"message_id": msgId,
+					"text": `${e.name}: ${e.message}`,
+				})
+			}
+		);
+		return;
+	}
+	await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/editmessagetext`,
+		{
+			"method": "POST",
+			"headers": {
+				"Content-Type": "application/json"
+			},
+			"body": JSON.stringify({
+				"chat_id": chatId,
+				"message_id": msgId,
+				"text": `\`${env.DOMAIN_NAME}/${fileName}\``,
+				"parse_mode": "markdown",
+				"reply_markup": {
+					"inline_keyboard": [[
+						{
+							"text": "delete",
+							"callback_data": "/delete"
+						},
+						{
+							"text": "open url",
+							"url": `${env.DOMAIN_NAME}/${fileName}`
+						}
+					]]
+				}
+			})
+		}
+	);
+	await env.images.put(fileName, response.body)
+}
+
+async function tgDeleteFile(replyText, replyChatId, replyMessageId, replyUserId) {
 	if (replyUserId != env.BOT_ID) {
 		return;
 	}
+	let url;
 	try {
-		var url = new URL(replyText);
+		url = new URL(replyText);
 	}
 	catch (e) {
 		return;
 	}
-	if (url.origin === env.DOMAIN_NAME) {
+	if (url.origin === env.DOMAIN_NAME || true) {
 		await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/editmessagetext`,
 			{
 				"method": "POST",
@@ -108,16 +208,16 @@ async function tgDeleteFile(replyText,replyChatId,replyMessageId,replyUserId) {
 }
 
 async function tgCallbackQuery(requestJson) {
-	var data = requestJson["callback_query"]["data"];
-	var cqid = requestJson["callback_query"]["id"];
-	
+	let data = requestJson.callback_query.data;
+	let cqid = requestJson.callback_query.id;
+
 	if (data === "/delete") {
-		var replyToMessageJson = requestJson["callback_query"]["message"];
+		let replyToMessageJson = requestJson.callback_query.message;
 		if (replyToMessageJson) {
-			var replyUserId = replyToMessageJson["from"]["id"];
-			var replyChatId = replyToMessageJson["chat"]["id"];
-			var replyMessageId = replyToMessageJson["message_id"];
-			var replyText = replyToMessageJson["text"];
+			let replyUserId = replyToMessageJson.from.id;
+			let replyChatId = replyToMessageJson.chat.id;
+			let replyMessageId = replyToMessageJson.message_id;
+			let replyText = replyToMessageJson.text;
 			await tgDeleteFile(
 				replyText,
 				replyChatId,
@@ -126,18 +226,18 @@ async function tgCallbackQuery(requestJson) {
 			);
 		}
 	}
-	fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/answercallbackquery?callback_query_id=${cqid}&text=deleted`)
+	fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/answercallbackquery?callback_query_id=${cqid}&text=deleted`);
 }
 
 async function tgReceiveCommand(requestJson) {
-	var text = requestJson["message"]["text"];
+	let text = requestJson.message.text;
 	if (text === "/delete") {
-		var replyToMessageJson = requestJson["message"]["reply_to_message"];
+		let replyToMessageJson = requestJson.message.reply_to_message;
 		if (replyToMessageJson) {
-			var replyUserId = replyToMessageJson["from"]["id"];
-			var replyChatId = replyToMessageJson["chat"]["id"];
-			var replyMessageId = replyToMessageJson["message_id"];
-			var replyText = replyToMessageJson["text"];
+			let replyUserId = replyToMessageJson.from.id;
+			let replyChatId = replyToMessageJson.chat.id;
+			let replyMessageId = replyToMessageJson.message_id;
+			let replyText = replyToMessageJson.text;
 			await tgDeleteFile(
 				replyText,
 				replyChatId,
@@ -148,7 +248,7 @@ async function tgReceiveCommand(requestJson) {
 	}
 }
 
-router.post("/telegram", async(request,ctx) => {
+router.post("/telegram", async (request, ctx) => {
 	if (request.headers.get("X-Telegram-Bot-Api-Secret-Token") !== env.TELEGRAM_VERIFY_TOKEN) {
 		return new Response("Forbidden.", {
 			"status": 403
@@ -156,55 +256,70 @@ router.post("/telegram", async(request,ctx) => {
 	}
 
 	if (!(request.headers.get("Content-Type") || "").includes("application/json")) {
-		return new Response("Not supported Content-Type.",{
+		return new Response("Not supported Content-Type.", {
 			"status": 400
 		});
 	}
-	var requestJson = await request.json();
-	if("callback_query" in requestJson) {
+
+	let requestJson = await request.json();
+	if ("callback_query" in requestJson) {
 		ctx.waitUntil(tgCallbackQuery(requestJson));
 		return new Response("");
-	}
-	else if(requestJson["message"]["text"] && requestJson["message"]["text"][0] === "/") {
+	} else if (requestJson.message.text && requestJson.message.text[0] === "/") {
 		ctx.waitUntil(tgReceiveCommand(requestJson));
 		return new Response("");
 	}
-	
 
-	var userId = requestJson["message"]["from"]["id"];
-	var chatId = requestJson["message"]["chat"]["id"];
-	var messageId = requestJson["message"]["message_id"];
-	var doc = requestJson["message"]["document"];
-	var photo = requestJson["message"]["photo"];
-	var video = requestJson["message"]["video"];
+
+	let userId = requestJson.message.from.id;
+	let chatId = requestJson.message.chat.id;
+	let messageId = requestJson.message.message_id;
+	let doc = requestJson.message.document;
+	let photo = requestJson.message.photo;
+	let video = requestJson.message.video;
+	let text = requestJson.message.text;
+	let text_url;
+	try {
+		text_url = new URL(text);
+	} catch {
+
+	}
+	console.log(text_url);
+	console.log(text);
 	if (!validUserId(userId)) {
 		return new Response("");
 	}
 	if (doc) {
 		ctx.waitUntil(tgPutFile(
-			doc["file_id"],
+			doc.file_id,
 			chatId,
 			messageId
 		));
 	} else if (photo) {
 		ctx.waitUntil(tgPutFile(
-			photo.pop()["file_id"],
+			photo.pop().file_id,
 			chatId,
 			messageId
 		));
 	} else if (video) {
 		ctx.waitUntil(tgPutFile(
-			video["file_id"],
+			video.file_id,
 			chatId,
 			messageId
 		));
+	} else if (text_url) {
+		ctx.waitUntil(urlPutFile(
+			text,
+			chatId,
+			messageId
+		))
 	}
 
 	return new Response("");
 })
 
-router.all("*", async() => {
-	return new Response("Not Found",{
+router.all("*", async () => {
+	return new Response("Not Found", {
 		"status": 404
 	});
 })
